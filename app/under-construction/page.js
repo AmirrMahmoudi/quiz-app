2c2c5e2f88613f41c56c71d62509da6649458bed
AmirrMahmoudi <amirhosssein.mahmoudi@gmail.com>
2023-11-22 16:17:05 +0330

    Implementation

---
+++ app/under-construction/page.js
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+export default function underConstraction() {
+  return (
+    <>
+      <main className={styles.main}>
+        <section className={styles.contact}>
+          <h1 className={styles.title}>خیلی شرمنده می باشیم</h1>
+          <h2 className={styles.subtitle}>
+            😊 در حال بروزرسانی هستیم لطفا مدتی دیگر مراجعه بفرمایید
+          </h2>
+        </section>
+      </main>
+    </>
+  );
+}
